3bf2ec08dd52be28a1a685869ce9d27c3155d8da
Hashino <iam.ravara@gmail.com>
2023-07-20 03:47:15 -0300

    removed webdev configs

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -7,11 +7,6 @@ end
 local b = null_ls.builtins
 
 local sources = {
-
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
   -- Lua
   b.formatting.stylua,
 
--- configs/overrides.lua
+++ configs/overrides.lua
@@ -26,13 +26,6 @@ M.mason = {
     "lua-language-server",
     "stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-
     -- c/cpp stuff
     "clangd",
     "clang-format",
