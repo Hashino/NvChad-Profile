a4803064e4d9de93468dcb903cf51023217143c8
Hashino <iam.ravara@gmail.com>
2023-07-20 03:32:49 -0300

    initial config

---
+++ configs/overrides.lua
@@ -0,0 +1,98 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "c",
+    "cpp",
+    "css",
+    "javascript",
+    "typescript",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+local function nvimtree_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '-',          api.tree.change_root_to_node,         opts('Change Directory'))
+    vim.keymap.set('n', '<BS>',       api.tree.change_root_to_parent,       opts('Up a Directory'))
+    vim.keymap.set('n', '<Left>',     api.node.navigate.parent_close,       opts('Close Directory'))
+    vim.keymap.set('n', '<Right>',    api.node.open.edit,                   opts('Open'))
+    vim.keymap.set('n', '?',          api.tree.toggle_help,                 opts('Help'))
+end
+-- git support in nvimtree
+M.nvimtree = {
+  on_attach = nvimtree_on_attach,
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+      glyphs = {
+        default = "󰈚",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+}
+
+return M
